a7266522cc0cb9b23f5b4563af370dbfc200f8fb
steven <tanguysteven15@gmail.com>
2020-12-01 15:02:39 +0100

    realisation des premier service pour la page globale

--- lib/service/GlobaleService.dart
+++ lib/service/GlobaleService.dart
@@ -1,24 +1,28 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+
 
 class GlobaleService{
   static final CollectionReference Eventcollection = FirebaseFirestore.instance.collection("Evenement");
   static Stream<QuerySnapshot> get Evenementstream {
-    return Eventcollection.orderBy("writtenDate", descending: true).snapshots();
+    return Eventcollection.orderBy("titre_fr", descending: true).snapshots();
   }
-  static Future<QuerySnapshot> RechercheEvenement() {
-    FirebaseFirestore.instance.collectionGroup("event").get();
+  static Future<QuerySnapshot> RechercheEvenement(String titreEvent) async {
+    Eventcollection.where("titre_fr", isEqualTo: titreEvent ).snapshots();
   }
-  static Future<DocumentReference> RechercheParLieu(String message) {
-    FirebaseFirestore.instance.collectionGroup("event").get();
+  static Future<DocumentReference> RechercheParLieu(String nomVille) async {
+    Eventcollection.where("ville", isEqualTo: nomVille ).snapshots();
 
   }
-  static Future<DocumentReference> RechercheParTheme(String message) {
-    FirebaseFirestore.instance.collectionGroup("event").get();
+  static Future<DocumentReference> RechercheParTheme(String nomTheme) async{
+    Eventcollection.where("thematiques", isEqualTo: nomTheme ).snapshots();
 
   }
-  static Future<DocumentReference> RechercheParDate(String message) {
-    FirebaseFirestore.instance.collectionGroup("event").get();
+  static Future<DocumentReference> RechercheParMotCle(String motcle) async {
+    Eventcollection.where("mots_cles_fr", isEqualTo: motcle ).snapshots();
+
+  }
+  static Future<DocumentReference> RechercheParDate(String date) async{
+    Eventcollection.where("dates", isEqualTo: date ).snapshots();
 
   }
 }
