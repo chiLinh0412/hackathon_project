76eed53fd9ff913d1e75d1ee3b9e54836628f09d
steven <tanguysteven15@gmail.com>
2020-12-01 18:07:44 +0100

    correction de Evenement et GlobaleService

--- lib/service/GlobaleService.dart
+++ lib/service/GlobaleService.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class GlobaleService {
-  String searchKey;
   static final CollectionReference Eventcollection =
       FirebaseFirestore.instance.collection("Evenements");
 
@@ -13,31 +12,30 @@ class GlobaleService {
       String nomVille, String nomTheme, String motcle, String date) {
     Stream<QuerySnapshot> retour;
     if (titreEvent != null && titreEvent != "") {
-      retour =
-          Eventcollection.where('titre_fr', isGreaterThanOrEqualTo: searchKey)
-              .where('titre_fr', isLessThan: searchKey + 'z')
+      retour = Eventcollection.where('titre_fr', isGreaterThanOrEqualTo: titreEvent)
+              .where('titre_fr', isLessThan: titreEvent + 'z')
               .snapshots();
     }
     if (nomVille != null && nomVille != "") {
-      retour = Eventcollection.where('ville', isGreaterThanOrEqualTo: searchKey)
-          .where('ville', isLessThan: searchKey + 'z')
+      retour = Eventcollection.where('ville', isGreaterThanOrEqualTo: nomVille)
+          .where('ville', isLessThan: nomVille + 'z')
           .snapshots();
     }
     if (nomTheme != null && nomTheme != "") {
       retour = Eventcollection.where('thematiques',
-              isGreaterThanOrEqualTo: searchKey)
-          .where('thematiques', isLessThan: searchKey + 'z')
+              isGreaterThanOrEqualTo: nomTheme)
+          .where('thematiques', isLessThan: nomTheme + 'z')
           .snapshots();
     }
     if (motcle != null && motcle != "") {
       retour = Eventcollection.where('mots_cles_fr',
-              isGreaterThanOrEqualTo: searchKey)
-          .where('mots_cles_fr', isLessThan: searchKey + 'z')
+              isGreaterThanOrEqualTo: motcle)
+          .where('mots_cles_fr', isLessThan: motcle + 'z')
           .snapshots();
     }
     if (date != null && date != "") {
-      retour = Eventcollection.where('dates', isGreaterThanOrEqualTo: searchKey)
-          .where('dates', isLessThan: searchKey + 'z')
+      retour = Eventcollection.where('dates', isGreaterThanOrEqualTo: date)
+          .where('dates', isLessThan: date + 'z')
           .snapshots();
     }
     return retour;
